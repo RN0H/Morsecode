1b6b0a007e893335e6f28fdb5650b7252f5e44ba
N0R4H <rohanpan27@gmail.com>
2022-04-10 15:27:10 +0530

    version-1

--- Morsecode.py
+++ Morsecode.py
@@ -1,3 +1,14 @@
+ #!/usr/bin/env python3 
+# -*- coding: utf-8 -*- 
+
+#================================================
+# Author : Rohan Panicker
+# Created Date: 10/4/22
+# version ='1.0'
+#================================================
+
+
+
 from tkinter import*
 import tkinter as tk
 from tkinter.ttk import*
